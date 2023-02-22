8010e85e4044020e39bb22e10a9e00ec9340f0ad
e1vi5 <e1vi5@qq.com>
2023-02-22 14:38:55 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,20 @@
+# print("hello world")    #字符串
+# print(23333)            #整数
+# print(2.3333)
+# print(True)
+# print(False)
+# print(())
+# print([])
+# print({})
+# """sdjf;jdsafjsdaf"""
+# print(23333, "哈哈哈哈")
+# print("哈哈" + "嘻嘻")  #字符串的拼接，只能同样的数据类型进行操作
+# print("哈哈哈" * 2)
+
+"""
+练习：通过代码获取两段内容，并且计算他们的长度的和
+"""
+
+a = input("请输入第一段内容：")
+b = input("请输入第二段内容：")
+print("两段字符串的长度是：", len(a) + len(b))
